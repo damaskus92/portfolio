9c5d9cf05694e1db416a09b032d72091bf2594a6
damaskus92 <damaskusuma92@gmail.com>
2026-01-14 13:30:48 +0700

    install primevue & tailwind

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <div class="card flex justify-center">
+    <Button label="Verify" class="mt-4" />
+  </div>
 </template>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,30 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+
+import Lara from '@primeuix/themes/lara'
 
 import App from './App.vue'
 import router from './router'
+
+import './style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+    options: {
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+  ripple: true,
+})
 
 app.mount('#app')
